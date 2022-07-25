ce84810a5eb669359675acb68f6060f910d9dd82
X21026 <x21026xx@aitech.ac.jp>
2022-07-25 09:47:18 +0900

    updata

--- portfolio.js
+++ portfolio.js
@@ -9,13 +9,11 @@ function fadeAnime(){
       $(this).removeClass('fadeUp');
       }
       });
-  }
-  
+}
   
 $(window).scroll(function (){
     fadeAnime();
 });
-  
   
 $(window).on('load', function(){
     fadeAnime();
@@ -26,3 +24,27 @@ $(function() {
         $('#menu-btn-check').prop('checked', false);
     });
 });
+
+$(function(){
+    var topBtn=$('#page_top');
+    topBtn.hide();
+      
+    //ボタンの表示設定
+    $(window).scroll(function(){
+      if($(this).scrollTop()>80){
+        // 画面を80pxスクロールしたら、ボタンを表示する
+        topBtn.fadeIn();
+      }else{
+        // 画面が80pxより上なら、ボタンを表示しない
+        topBtn.fadeOut();
+      }
+    });
+    
+    // ボタンをクリックしたら、スクロールして上に戻る
+    topBtn.click(function(){
+      $('body,html').animate({
+      scrollTop: 0},500);
+      return false;
+    });
+    
+    });
